566278fcfe005f96049ca98c781eb009cf3092ec
renatex333 <renato9302@hotmail.com>
2024-09-06 19:10:28 -0300

    Adding unit testing to assure lambda function and api are deployed and working.

--- tests/test.py
+++ tests/test.py
@@ -2,10 +2,23 @@ import os
 import io
 import json
 import boto3
+import requests
 from dotenv import load_dotenv
 
 def main():
     load_dotenv()
+
+    # Payload
+    payload = json.dumps({
+        "age": 42,
+        "job": "entrepreneur",
+        "marital": "married",
+        "education": "primary",
+        "balance": 558,
+        "housing": "yes",
+        "duration": 186,
+        "campaign": 2
+    })
 
     # Create a Boto3 client for AWS Lambda
     lambda_client = boto3.client(
@@ -20,7 +33,15 @@ def main():
 
     # Test the function
     test_function_basic_reponse(lambda_client, function_name)
-    test_function_with_payload(lambda_client, function_name)
+    test_function_with_payload(lambda_client, function_name, payload)
+
+    # Close the Lambda client
+    lambda_client.close()
+
+    # API Gateway URL
+    url = os.getenv("API_GATEWAY_URL")
+    test_api_basic_response(url)
+    test_api_with_payload(url, payload)
 
 def test_function_basic_reponse(lambda_client, function_name):
 
@@ -29,25 +50,16 @@ def test_function_basic_reponse(lambda_client, function_name):
     response = function_invoke(lambda_client, function_name)
     assert response is not None, "Function did not return a response"
     response = json.loads(response)
+    print("Test function basic response")
     print(response)
     assert response["message"] == "No body in the request", "Function did not return the expected message"
 
-def test_function_with_payload(lambda_client, function_name):
-    payload = {
-        "body": json.dumps({
-            "age": 42,
-            "job": "entrepreneur",
-            "marital": "married",
-            "education": "primary",
-            "balance": 558,
-            "housing": "yes",
-            "duration": 186,
-            "campaign": 2
-        })
-    }
+def test_function_with_payload(lambda_client, function_name, payload):
+    payload = json.dumps({"body": payload})
     response = function_invoke(lambda_client, function_name, payload)
     assert response is not None, "Function did not return a response"
     response = json.loads(response)
+    print("Test function with payload")
     print(response)
     assert response["prediction"] != "None", "Function did not return a prediction"
 
@@ -69,7 +81,7 @@ def function_invoke(lambda_client, function_name, payload="") -> dict:
         response = lambda_client.invoke(
             FunctionName=function_name,
             InvocationType="RequestResponse",
-            Payload=json.dumps(payload),
+            Payload=payload,
         )
 
         return io.BytesIO(response["Payload"].read()).read().decode("utf-8")
@@ -77,6 +89,24 @@ def function_invoke(lambda_client, function_name, payload="") -> dict:
     except Exception as e:
         print(e)
         return None
+
+def test_api_basic_response(url):
+    response = requests.post(url)
+    status_code = response.status_code
+    response = json.loads(response.text)
+    print("Test API basic response")
+    print(response)
+    assert status_code == 200, "Status code is not 200"
+    assert response["message"] == "No body in the request", "API did not return the expected message"
+
+def test_api_with_payload(url, payload):
+    response = requests.post(url, json=json.loads(payload))
+    status_code = response.status_code
+    response = json.loads(response.text)
+    print("Test API with payload")
+    print(response)
+    assert status_code == 200, "Status code is not 200"
+    assert response["prediction"] != "None", "API did not return a prediction"    
 
 if __name__ == "__main__":
     main()
